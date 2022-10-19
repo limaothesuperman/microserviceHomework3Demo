fc1bead9d0fefc1a1a89d8634a75bb641abdd86a
renqingyang <rq625230882@gmail.com>
2022-10-19 10:55:29 -0500

    homework explain

--- search/src/main/java/com/example/search/SearchApplication.java
+++ search/src/main/java/com/example/search/SearchApplication.java
@@ -7,10 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan(basePackages = {"com.example.common", "com.example.search"})
 public class SearchApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SearchApplication.class, args);
     }
-
 }
