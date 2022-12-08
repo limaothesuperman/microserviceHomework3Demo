7d9058eb16ac26c369b05d4576f87b47273364a6
renqingyang <rq625230882@gmail.com>
2022-12-08 10:57:35 -0600

    homework explain

--- src/test/java/com/example/weather/WeatherApplicationTests.java
+++ src/test/java/com/example/weather/WeatherApplicationTests.java
@@ -1,12 +1,10 @@
 package com.example.weather;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WeatherApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
